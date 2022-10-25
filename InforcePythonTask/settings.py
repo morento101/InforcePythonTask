8f6488d12c75e004f81e8c7029afe788a852e6fb
Yaroslav Kovaliv <tlcees@gmail.com>
2022-10-25 19:35:46 +0300

    added jwt authentication

--- InforcePythonTask/settings.py
+++ InforcePythonTask/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'menu',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,6 @@ WSGI_APPLICATION = 'InforcePythonTask.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -131,3 +130,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'authentication.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
--- menu/views.py
+++ menu/views.py
@@ -1,7 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 
 class HelloAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request):
         return Response(data={"Hello": "World"})
